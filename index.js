5afa9e5e63c0de54694aaae3ef2d2a87b6786d14
Mario Gomez <mgomez15@seattlecentral.edu>
2020-07-26 17:50:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(Object, key, value){
+  
+  //return value with the original key value pairs 
+  //and the new key value pairs
+  
+ Object.assign({key}{ ['value'] });
+ 
+ Object.assign({}, meals, { breakfast: ['oatmeal', 'banana'] })
+  
+}
+  
